a266e19f2d5d8c3eb9821e81badd8c7f72018065
Yogendra Dikhit <yogendradikhit1996@gmail.com>
2022-05-03 20:16:12 +0530

    Updated contact references

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -7,7 +7,9 @@ import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import {
   faFacebook,
   faGithub,
+  faInstagram,
   faLinkedin,
+  faWhatsapp,
 } from '@fortawesome/free-brands-svg-icons';
 
 const SideBar = () => {
@@ -37,18 +39,44 @@ const SideBar = () => {
       </nav>
       <ul>
         <li>
-          <a href="#" rel="noreferrer" target="_blank">
+          <a
+            href="https://linkedin.com/in/yogendra-dikhit"
+            rel="noreferrer"
+            target="_blank"
+          >
             <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
           </a>
         </li>
         <li>
-          <a href="#" rel="noreferrer" target="_blank">
+          <a
+            href="https://github.com/yogendra-dikhit"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a href="https://wa.me/917489990973" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faWhatsapp} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://fb.com/yogendra.dikhit.9"
+            rel="noreferrer"
+            target="_blank"
+          >
             <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
           </a>
         </li>
         <li>
-          <a href="#" rel="noreferrer" target="_blank">
-            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          <a
+            href="https://instagram.com/yogendra.dikhit.9"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
           </a>
         </li>
       </ul>
