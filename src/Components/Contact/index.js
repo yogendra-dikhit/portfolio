920f18d10a084870053f36499992d19a7b25f85d
Yogendra Dikhit <yogendradikhit1996@gmail.com>
2022-05-03 20:16:45 +0530

    Added Contact page

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,5 +1,116 @@
+import { useEffect, useRef, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import Loader from 'react-loaders';
+import './index.scss';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const formRef = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+
+  const sendMail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('SERVICE_ID', 'template_f87u1oa', formRef.current, 'PUBLIC_KEY')
+      .then(
+        () => {
+          alert('Message successfully sent!');
+          window.location.reload(false);
+        },
+        (error) => {
+          alert('Failed to send the message, please try again!!');
+        }
+      );
+  };
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              word={'Contact me'}
+              letterClass={letterClass}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={formRef} onSubmit={sendMail}>
+              <ul>
+                <li className="half">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email "
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    id="message"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="Send" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="map-info">
+          Yogendra Dikhit,
+          <br />
+          Mumbai, Maharashtra India.
+          <br />
+          <span>yogendradikhit1996@gmail.com</span>
+        </div>
+        <div className="map-container">
+          <MapContainer center={[19.076, 72.8777]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[19.076, 72.8777]}>
+              <Popup>
+                Yogendra lives here, come over for a cup of coffee :)
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
 };
 
 export default Contact;
