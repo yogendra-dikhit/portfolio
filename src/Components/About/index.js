b0bdb8024d09cd7d0f48e911e6ef203a38a4229a
Yogendra Dikhit <yogendradikhit1996@gmail.com>
2022-05-03 16:15:55 +0530

    Added About page

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,7 +1,75 @@
+import {
+  faAngular,
+  faCss3,
+  faHtml5,
+  faJava,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const About = () => {
-  return <div>About</div>;
+  const [letterClass, setLetterClass] = useState('text-animate');
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              word={'About me'}
+              letterClass={letterClass}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm very ambitious full-stack developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, fun-loving, problem-solver and tech-obsessed!!!
+          </p>
+        </div>
+        <div className="cube-container">
+          <div className="cube-spinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJava} color="#EC4D28" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
 };
 
 export default About;
